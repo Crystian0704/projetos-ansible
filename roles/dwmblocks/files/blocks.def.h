816a8a20a4bc334bc5922bedb66fd9847c249da2
Crystian <crystian.alexsander@gmail.com>
2023-09-04 01:10:34 -0300

    refactor: remove status de ip e diminui o tamanho do gap

--- roles/dwmblocks/files/blocks.def.h
+++ roles/dwmblocks/files/blocks.def.h
@@ -1,11 +1,9 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
         /*Icon*/        /*Command*/                                   /*Update Interval*/     /*Update Signal*/
-        {"🌐 ", "hostname -I | awk '{print $1}'", 60, 0}, // Usando emoji para representar o endereço IP
-
         {"🧠 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0}, // Usando emoji para representar memória
 
-        {"📅 ", "date '+%d/%m/%Y %H:%M'", 5, 0}, // Usando emoji para representar data e alterando o formato da data
+        {"📅 ", "date '+%d/%m %H:%M'", 5, 0}, // Usando emoji para representar data e alterando o formato da data
 
         {"🔋 ", "acpi | awk '{ print $4 }' | cut -d, -f1", 30, 0}, // Usando emoji para representar a bateria
 
@@ -14,5 +12,5 @@ static const Block blocks[] = {
 };
 
 // sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim[] = "|";
+static unsigned int delimLen = 3;
